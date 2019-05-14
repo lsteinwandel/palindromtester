5f80b9bd5d353ccdaace49d4c76348cb2ee1b1e0
lsteinwandel <50633036+lsteinwandel@users.noreply.github.com>
2019-05-14 22:36:49 +0200

    Update

--- 2019-05-14-Test-Elbformat.py
+++ 2019-05-14-Test-Elbformat.py
@@ -3,16 +3,19 @@
 # Importieren der tkinter library
 from tkinter import *
 from tkinter import ttk
+import re
 
 # Funktion zum testen der Eingabe
 def palindromtester(x):
     y = x[::-1]
     if len(x) < 2: # Abfrage zur Länge der Eingabe
-        return "Bitte wählen sie einen Namen mit mindestens zwei Buchstaben!"
+        return "Bitte wählen sie einen Wort mit mindestens zwei Buchstaben!"
+    elif bool(re.match("^[A-Za-z]*$", x)) == 0: # Suche nach verbotenen Symbolen
+        return "Bitte geben sie ein gültiges Wort ein"
     elif x.lower() == y.lower(): # Vergleich des Wortes vor- und rückwärts geschrieben
-        return "Bei dem Namen " + x + " handelt es sich um ein Palindrom!"
+        return "Bei dem Wort " + x + " handelt es sich um ein Palindrom!"
     else:
-        return "Der Name " + x + " ist kein Palindrom!"
+        return "Das Wort " + x + " ist kein Palindrom!"
 
 # Funktion für den Button
 def ergebnis():
@@ -25,7 +28,7 @@ gui = Tk() # Erstellen des Fensters
 gui.title("Palindromtester von Leo Steinwandel")
 gui.geometry("400x100")
 gui.resizable(0, 0)
-text = ttk.Label(text="Wie lautet der Name:") # Erstellen des ersten Labels
+text = ttk.Label(text="Wie lautet das Wort:") # Erstellen des ersten Labels
 text.grid()
 text.place(x=200, y=20, anchor="center")
 textinput = StringVar()
